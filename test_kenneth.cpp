6fcd2b9ee5ad7acf01c4fb0a3c57b1ffc693cedf
KennethACS <00118026@uca.edu.sv>
2026-07-07 08:06:13 -0600

    feature: test de funcionalidad completa

--- test_kenneth.cpp
+++ test_kenneth.cpp
@@ -27,15 +27,25 @@ int main()
     cout<<"Tamaño array: " <<tamano <<endl;
     obtenerPreguntaAleatoria(preguntas_entrada,respuestas_entrada,preguntas_salida,respuestas_salida, tamano);
 
-    cout<<"Distribucion aleatoria con correspondencia generada: ";
+    cout<<"Distribucion aleatoria con correspondencia generada: " <<endl;
     for (int i = 0; i < 5; i++)
     {
         cout<<preguntas_salida[i] <<" ";
         cout<<respuestas_salida[i] <<endl;
     }
-    
 
+    cout<<"Titulos de las preguntas: " <<endl;
+    string preguntas_entrada[] = {"1","2","3","4","5","6","7","8","9","10"};
+    for (int i = 0; i < 10; i++)
+    {
+        mostrar_pregunta(i, preguntas_entrada);
+    }
 
-
+    cout<<"verificar respuesta:";
+    string valores[] = {"a", "b", "c", "d", "e"};
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<verificar_respuesta(i, valores, "a"); //Entrada quemada para verificar comportamiento
+    }
     return 0;
 }
