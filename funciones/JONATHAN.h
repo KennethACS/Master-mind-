495571115fc0b7474c4c8d88778f4b08d281eb33
KennethACS <00118026@uca.edu.sv>
2026-07-07 22:28:55 -0600

    Ordenar archivos

---
+++ funciones/JONATHAN.h
@@ -0,0 +1,7 @@
+#ifndef JONATHAN_H
+#define JONATHAN_H
+using namespace std;
+void finalizarjuego(float puntaje_jugador1, float puntaje_jugador2, bool jugador2);
+void mostrarpuntaje(float puntaje_jugador1, float puntaje_jugador2, bool jugador2);
+
+#endif
